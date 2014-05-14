803391c20a7f03f3682a9e15a403cb1ea6da8366
Vanessa Barbosa <vanessabmdf@gmail.com>
2014-05-14 19:22:33 -0300

    Red for test calculaCoeficiente

---
+++ prova2B/src/resolucaoProva/Linha2D.java
@@ -0,0 +1,18 @@
+package resolucaoProva;
+
+public class Linha2D {
+
+	private Ponto l1a;
+	private Object l2a;
+
+	public Linha2D(Ponto l1a, Ponto l1b) {
+		this.l1a = l1a;
+		this.l2a = l2a;
+		
+	}
+
+	public static boolean interseccao(Linha2D l1, Linha2D l2) {
+		return true;
+	}
+
+}
---
+++ prova2B/src/resolucaoProva/Ponto.java
@@ -0,0 +1,12 @@
+package resolucaoProva;
+
+public class Ponto {
+
+	private int x,y;
+
+	public Ponto(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
